8a14d40d351287ac7b02d831e23f23f0edc4978f
Gaelpem <pembelegael@gmail.com>
2025-03-19 19:29:09 +0100

    essai

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,73 @@
 <template>
-  <p>Compteur : {{ count }} </p>
+  <form action="" @submit.prevent="addTodo">
 
 
-  <button v-on:click="increment">Incrementer</button>
-  <button v-on:click="decrement">Decrementer</button>
+    <fieldset role="group">
 
-<ul>
-  <li v-for="movie in movies">
-    {{ movie }} <button @click="deleteMovie(movie)">Supprimer</button>
+      <input v-model="newTodo"
+      type="text" placeholder="Tache à effectuer">
 
-  </li>
-</ul>
+      <button :disabled="newTodo.length == 0" >Ajouter</button>
+
+    </fieldset>
+  </form>
+
+
+  <div v-if="todos.length == 0">Vous avez pas de tâches à faire </div>
+
+  <div v-else>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.date" :class="{completed: todo.completed}">
+      <label>
+        <input type="checkbox" :checked="todo.completed" v-model="todo.completed">
+        {{ todo.title }}
+      </label>
+      </li>
+    </ul>
+   <Checkbox label="Bonjour"/>
+  </div>
+
+
+  
 </template>
 
 
 <script setup>
 import { ref } from 'vue';
+import Checkbox from './Checkbox.vue'
 
-  const count = ref(1)
+const newTodo = ref('')
 
-  const movies = ref([
-    'Lapin',
-    'Blanc', 
-    'Voisin'
-  ]);
-  const increment = ()=>{
-    count.value++
-  }
+const todos = ref([{
+  title: 'Tâche de test', 
+  completed: true, 
+  date:1
+},{
+  title: 'Tâche à faire ', 
+  completed: false, 
+  date:2
+}])
 
-  const decrement = () =>{
-    count.value--
-  }
+const addTodo = () =>{
 
-const deleteMovie = (movie) =>{
-  movies.value = movies.value.filter(m => m !== movie)
+  todos.value.push({
+    title : newTodo.value,
+    completed: false,
+    date: Date.now()
+
+  })
+
+const sortedTodos = () =>{
+  z
+}
 }
 </script>
 
+
 <style>
-.active{
-  color : red; 
+.completed{
+  opacity: 0.5;
+  text-decoration: line-through;
 }
 </style>
---
+++ src/Checkbox.vue
@@ -0,0 +1,14 @@
+<template>
+<input type="checkbox">
+Lorem ipsum dolor sit amet, 
+</template>
+
+<script setup>
+defineProps({
+
+    defineP
+})
+</script>
+
+<style>
+</style>
