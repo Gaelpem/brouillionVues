54e3efb168a0dfe978ec74e52850203154e2d63a
Gaelpem <pembelegael@gmail.com>
2025-03-25 16:15:52 +0100

    entrainement

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <input v-model="recherche" placeholder="Recherche">
+  <form @submit.prevent="addItem">
+    <p>What do you need ?</p>
+    <input v-model="addFruit" placeholder="Choisissez un fruit" >
+    <p>How much you need</p>
+    <input type="number" v-model="addNumber" placeholder="Un nombre" >
+    <button type="submit">Ajouter</button>
+  </form>
+  <button @click="deleteItem">Supprimer</button>
   <ul>
-    <li v-for="nom in  firlterNom" :key="nom">{{ nom }}</li>
+    <li v-for="(item , index) in shoppingList" :key ='index'>
+  {{ item.name}}, {{ item.number}} </li>
   </ul>
 </template>
-
-
+ 
 <script>
 export default{
-    data(){
-      return{
-        recherche:'',
-        noms:[]
-      }
-    }, 
-    computed:{
-      firlterNom(){
-         return this.noms.filter((nom)=> nom.includes(this.recherche))
-      }
-      
-    }, 
-    mounted(){
-     return this.noms = ['Lelia', 'Jeremie', 'Gael', 'Boubacar']
+  data(){
+    return{
+      addFruit:'',
+      addNumber:'', 
+      shoppingList: []
     }
+  }, 
+  methods:{
+    addItem(){
+    if(this.addFruit && this.addNumber){
+      let item = {
+        name: this.addFruit,
+        number: this.addNumber
+      }
+      this.shoppingList.push(item)
+      this.addFruit = ''
+      this.addNumber = ''
 
+        }
+    
+    },
+    deleteItem(){
+      if(this.shoppingList.length > 0){
+        this.shoppingList.pop()
+      }
+    }
+  }
 }
 </script>
